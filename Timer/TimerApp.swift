21d7bbc0ae2cd619412b68ed7c72b54ed355fddd
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-19 09:35:13 +0200

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Skorobogatow, Christian on 19/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
